84eddf370c644ad42584b90ce727b583676ea8dc
Jingwen Chen <jin@crypt.sg>
2019-01-01 18:40:26 -0500

    Fix integration tests' incorrect paths

--- tests/integration/simple_integration_test.sh
+++ tests/integration/simple_integration_test.sh
@@ -6,14 +6,14 @@ set -o pipefail
 readonly SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
 # try to run it once here to bootstrap
-$SCRIPT_DIR/../third_party/coursier/coursier
+$SCRIPT_DIR/../../third_party/coursier/coursier
 
 # java_binary test
 bazel build //tests:java_binary_test_deploy.jar
-jar tf $SCRIPT_DIR/../bazel-bin/tests/java_binary_test_deploy.jar | sort > /tmp/java_binary_deploy_jar.actual
+jar tf $SCRIPT_DIR/../../bazel-bin/tests/java_binary_test_deploy.jar | sort > /tmp/java_binary_deploy_jar.actual
 diff $SCRIPT_DIR/java_binary_deploy_jar.golden /tmp/java_binary_deploy_jar.actual
 
 # android_binary test
 bazel build //tests:android_binary_test
-jar tf $SCRIPT_DIR/../bazel-bin/tests/android_binary_test_deploy.jar | sort > /tmp/android_binary_deploy_jar.actual
+jar tf $SCRIPT_DIR/../../bazel-bin/tests/android_binary_test_deploy.jar | sort > /tmp/android_binary_deploy_jar.actual
 diff $SCRIPT_DIR/android_binary_deploy_jar.golden /tmp/android_binary_deploy_jar.actual

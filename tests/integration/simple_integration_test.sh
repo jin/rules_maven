b3df0c4e09b17273b1dfb9b85a62d6184abeff7f
Jingwen Chen <jin@crypt.sg>
2019-01-01 19:35:02 -0500

    fix integration tests again

--- tests/integration/simple_integration_test.sh
+++ tests/integration/simple_integration_test.sh
@@ -9,11 +9,11 @@ readonly SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 $SCRIPT_DIR/../../third_party/coursier/coursier
 
 # java_binary test
-bazel build //tests:java_binary_test_deploy.jar
-jar tf $SCRIPT_DIR/../../bazel-bin/tests/java_binary_test_deploy.jar | sort > /tmp/java_binary_deploy_jar.actual
+bazel build //tests/integration:java_binary_test_deploy.jar
+jar tf $SCRIPT_DIR/../../bazel-bin/tests/integration/java_binary_test_deploy.jar | sort > /tmp/java_binary_deploy_jar.actual
 diff $SCRIPT_DIR/java_binary_deploy_jar.golden /tmp/java_binary_deploy_jar.actual
 
 # android_binary test
-bazel build //tests:android_binary_test
-jar tf $SCRIPT_DIR/../../bazel-bin/tests/android_binary_test_deploy.jar | sort > /tmp/android_binary_deploy_jar.actual
+bazel build //tests/integration:android_binary_test
+jar tf $SCRIPT_DIR/../../bazel-bin/tests/integration/android_binary_test_deploy.jar | sort > /tmp/android_binary_deploy_jar.actual
 diff $SCRIPT_DIR/android_binary_deploy_jar.golden /tmp/android_binary_deploy_jar.actual
